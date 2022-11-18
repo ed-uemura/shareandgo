d2bd28451a3cffbdf91e68bcb1b1b27af1f2c733
ed-uemura <shojiuemurae@student.douglascollege.ca>
2022-11-18 12:16:56 -0800

    Baseline v0.26

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Credentials', true);
 	return next();
   });
-app.use(cors({ credentials:true }));
+app.use(cors());
 
 // Connecting to MongoDB
 mongoose.connect(
